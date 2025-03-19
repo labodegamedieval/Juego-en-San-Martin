2eae20cc6b892f970e1fd7c3113e479138a083cf
labodegamedieval <labodegamedieval@gmail.com>
2025-03-19 19:21:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,22 @@
-const CACHE_NAME = "aventura-gonzalo-v1";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/sounds/musica-medieval.mp3",
-    "/images/portada.jpg",
-    "/images/gonzalo.jpg",
-    "/images/icon-192.png",
-    "/images/icon-512.png"
-];
-
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log("Cache abierta");
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open("aventura-gonzalo-v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles/style.css",
+        "/scripts/script.js",
+        "/sounds/musica-medieval.mp3",
+        "/images/portada.jpg",
+        "/images/icon-192.png",
+        "/images/icon-512.png"
+      ]);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
